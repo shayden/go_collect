f80d4966144c256a13fd612b8415941b69e971a6
Sean Hayden <ctordtor@outlook.com>
2019-12-29 13:00:31 -0500

    "borrowed" code from go-exif to return exif data

--- collector.go
+++ collector.go
@@ -1,24 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/dsoprea/go-exif"
 )
+
+type ifdEntry struct {
+	IfdPath     string                `json:"ifd_path"`
+	FqIfdPath   string                `json:"fq_ifd_path"`
+	IfdIndex    int                   `json:"ifd_index"`
+	TagId       uint16                `json:"tag_id"`
+	TagName     string                `json:"tag_name"`
+	TagTypeId   exif.TagTypePrimitive `json:"tag_type_id"`
+	TagTypeName string                `json:"tag_type_name"`
+	UnitCount   uint32                `json:"unit_count"`
+	Value       interface{}           `json:"value"`
+	ValueString string                `json:"value_string"`
+}
 
 // MediaFile describes a media file
 type MediaFile struct {
-	absolutePath string
-	fileName     string
-	extension    string
-	exifData     string
-	drivename    string
-	sha256       []byte
-	size         int64
+	AbsolutePath string `json:"absolute_path"`
+	FileName     string `json:"file_name"`
+	Extension    string `json:"extension"`
+	ExifData     string `json:"exif_data"`
+	Drivename    string `json:"drive_name"`
+	Sha256       []byte `json:"sha256"`
+	Size         int64  `json:"size_in_kb"`
 }
 
 var files []MediaFile
+var drivename string
 
 func walk(path string, info os.FileInfo, err error) error {
 	if err != nil {
@@ -29,19 +47,72 @@ func walk(path string, info os.FileInfo, err error) error {
 	switch strings.ToLower(filepath.Ext(path)) {
 	case ".jpg", ".jpeg", ".gif", ".png", ".mov", ".mp4":
 		files = append(files, MediaFile{
-			absolutePath: path,
-			fileName:     info.Name(),
-			extension:    filepath.Ext(path),
-			drivename:    filepath.VolumeName(path),
-			size:         info.Size() >> 10,
-			sha256:       HashFile(path),
+			AbsolutePath: path,
+			FileName:     info.Name(),
+			Extension:    filepath.Ext(path),
+			Drivename:    drivename,
+			Size:         info.Size() >> 10,
+			Sha256:       HashFile(path),
+			ExifData:     parseExifData(path),
 		})
 	}
 	return nil
 }
 
+var entries []ifdEntry
+
+//basically stolen from main.go in go-exif
+func parseExifData(path string) string {
+	im := exif.NewIfdMappingWithStandard()
+	ti := exif.NewTagIndex()
+
+	entries := make([]ifdEntry, 0)
+	visitor := func(fqIfdPath string, ifdIndex int, tagId uint16, tagType exif.TagType, valueContext exif.ValueContext) (err error) {
+
+		ifdPath, err := im.StripPathPhraseIndices(fqIfdPath)
+
+		it, _ := ti.Get(ifdPath, tagId)
+
+		valueString := ""
+		var value interface{}
+		if tagType.Type() == exif.TypeUndefined {
+			value, _ = exif.UndefinedValue(ifdPath, tagId, valueContext, tagType.ByteOrder())
+			valueString = fmt.Sprintf("%v", value)
+		} else {
+			valueString, _ = tagType.ResolveAsString(valueContext, true)
+			value = valueString
+		}
+
+		entry := ifdEntry{
+			IfdPath:     ifdPath,
+			FqIfdPath:   fqIfdPath,
+			IfdIndex:    ifdIndex,
+			TagId:       tagId,
+			TagName:     it.Name,
+			TagTypeId:   tagType.Type(),
+			TagTypeName: tagType.Name(),
+			UnitCount:   valueContext.UnitCount(),
+			Value:       value,
+			ValueString: valueString,
+		}
+
+		entries = append(entries, entry)
+
+		return nil
+	}
+
+	f, _ := os.Open(path)
+	data, _ := ioutil.ReadAll(f)
+	rawExif, _ := exif.SearchAndExtractExif(data)
+	exif.Visit(exif.IfdStandard, im, ti, rawExif, visitor)
+
+	exifData, _ := json.Marshal(entries)
+	return string(exifData)
+}
+
 // WalkPath Returns a list of media files starting at root
-func WalkPath(root string) []MediaFile {
+func WalkPath(root, dname string) []MediaFile {
+	drivename = dname
 	err := filepath.Walk(root, walk)
 	if err != nil {
 		fmt.Println(err)
--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+)
 
 func main() {
-	files := WalkPath(".")
+	// I couldn't figure out a nice and clean way for figuring out automatically
+	// what the volume name of the current directory actually was -- just ask
+	// the user
+	drive := flag.String("drive", "NA", "REQUIRED: The name of the current drive being searched.")
+	rootDir := flag.String("root", ".", "The name of the directory being searched.")
+	flag.Parse()
+	if *drive == "NA" {
+		fmt.Println("Error: Drive name required")
+		return
+	}
+
+	files := WalkPath(*rootDir, *drive)
 	for _, file := range files {
-		fmt.Println(file)
+		data, _ := json.Marshal(file)
+		fmt.Println(string(data))
 	}
 
 }
